0ce3634637528db313cc14846ea07988079b1d98
Colin <cwrmcolin@gmail.com>
2023-01-24 16:18:32 -0800

    Fixed the proc file name that was being used

--- proc_count.c
+++ proc_count.c
@@ -4,7 +4,7 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 
-static int show()
+static int proc_count_show(struct seq_file *m, void *v)
 {
 	struct task_struct *task;
 	int count = 0;
@@ -19,14 +19,14 @@ static int show()
 
 static int __init proc_count_init(void)
 {
-	proc_create_single("proc_count", 0, NULL, show);
+	proc_create_single("count", 0, NULL, proc_count_show);
 	pr_info("proc_count: init\n");
 	return 0;
 }
 
 static void __exit proc_count_exit(void)
 {
-	remove_proc_entry("proc_count", NULL);
+	remove_proc_entry("count", NULL);
 	pr_info("proc_count: exit\n");
 }
 
